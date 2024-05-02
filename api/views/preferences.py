0aaad96b4ccb8143d49963bd623463af8afde85e
Dan Watson <dcwatson@gmail.com>
2024-05-02 17:41:49 -0400

    Fix preferences API return type

--- api/views/preferences.py
+++ api/views/preferences.py
@@ -7,5 +7,5 @@ from hatchway import api_view
 
 @scope_required("read:accounts")
 @api_view.get(by_alias=True)
-def preferences(request: HttpRequest) -> dict:
+def preferences(request: HttpRequest) -> schemas.Preferences:
     return schemas.Preferences.from_identity(request.identity)
